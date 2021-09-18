38ab966f6f278b9b14d2e4d628df1a9d8e32726b
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-09-18 12:25:53 +0300

    add user router logging

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,15 +1,11 @@
-import express from "express";
+import express from 'express';
+import { logMiddleware } from '../middlewares/logMiddleware.js';
 const userRouter = express.Router();
 
-// middleware for user router
-userRouter.use(async (req, res, next) => {
-  console.timeStamp("user router call start " + req.method);
-  await next();
-  console.timeStamp("user router call end " + req.method);
-});
+userRouter.use(logMiddleware);
 
-userRouter.get("/", (req, res) => {
-  res.json(["a", "b", "c"]);
+userRouter.get('/', (req, res) => {
+  res.json(['a', 'b', 'c']);
 });
 
 export default userRouter;
